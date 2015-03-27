7f64a2bef1d5eacb536bf26edf797c715fc1652f
ikaz <icastillejos@gmail.com>
2015-03-26 21:25:49 -0700

    Moved to a different repo

--- Scala/HelloWorld/src/Complex.scala
+++
@@ -1,6 +0,0 @@
-class Complex (real: Double, val imaginary: Double) {
-  def re = real
-  def im = imaginary
-  override def toString() = 
-    "" + re + (if (im < 0 ) "" else "+") + im + "i"
-}
--- Scala/HelloWorld/src/HelloWorld.scala
+++
@@ -1,8 +0,0 @@
-/**
- * @author icastillejos
- */
-object HelloWorld {
-    def main(args: Array[String]){
-      println("Hello World!")
-    }
-}
